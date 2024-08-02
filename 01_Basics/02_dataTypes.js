e7a9cbfa3dae1b0c89aa3e1eaeae71b613508864
krishna1632 <krishna163208@gmail.com>
2024-08-02 09:23:15 +0530

    Yesterday task done

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -2,3 +2,14 @@
 
 console.log(typeof null); // object
 console.log(typeof undefined); // undefined
+
+/*  Primitive Data Types:
+
+    7 Types: String, Number, Boolean, null, undefined, Symbol,Bigint
+
+    Reference Data Types:
+
+    Arrays, Objects, Functions
+
+*/
+ 
